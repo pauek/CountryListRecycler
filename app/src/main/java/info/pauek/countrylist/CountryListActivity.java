73b459405473bec75bd975d60be97f68833ee025
Pau Fernández <pau.fernandez@upc.edu>
2018-10-09 13:31:50 +0200

    Ja tenim una llista fictícia de 200 països.

--- app/src/main/java/info/pauek/countrylist/CountryListActivity.java
+++ app/src/main/java/info/pauek/countrylist/CountryListActivity.java
@@ -2,12 +2,35 @@ package info.pauek.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CountryListActivity extends AppCompatActivity {
+
+    private RecyclerView country_list_view;
+    private List<String> countries;
+    private CountryListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_list);
+
+        countries = new ArrayList<>();
+        countries.add("Portugal");
+        countries.add("Spain");
+        countries.add("France");
+        for (int i = 0; i < 200; i++) {
+            countries.add("Country " + i);
+        }
+
+        country_list_view = findViewById(R.id.country_list_view);
+        country_list_view.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new CountryListAdapter(this, countries);
+        country_list_view.setAdapter(adapter);
     }
 }
